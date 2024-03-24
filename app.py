fd991164d5bb04f405702f902dfd594ad7129845
netorolo89 <83844752+netorolo89@users.noreply.github.com>
2024-03-24 13:39:04 -0600

    "formato graficas"

--- app.py
+++ app.py
@@ -4,56 +4,71 @@ import plotly_express as px
 import calendar
 prod = pd.read_csv(
     r'C:\Users\rodri\OneDrive\Desktop\TripleTen\Sprint5\Proyecto\prod_ana_RS\notebooks\Prod_Asig_Worked.csv')
-st.title('Production Tracker')
+st.title('Seguimiento a la Producción')
 st.header('Comportamiento de la producción de Hidrocarburos (2016-2024)')
-st.subheader('Pendiente por definir...TERMINA AL FINAL')
+st.subheader('En esta aplicación se puede visualizar el comportamiento de la producción de aceite, gas, y agua de las asignaciones pertenecientes a las cuencas del sureste')
 st.divider()
-st.markdown("Escribir indicaciones de como interactuar")
-asig = st.selectbox('Seleccionar asignación a analizar',
+st.subheader('Indicaciones')
+st.markdown("1. Seleccionar la asignación a revisar")
+st.markdown(
+    "2. Puede incluir la revisión a un pozo, seleccionando la casilla Revisar Pozo")
+st.markdown("3. Dar click en el botón Revisar")
+st.divider()
+asig = st.selectbox('Seleccionar asignación',
                     prod['Asignación_o_Contrato'].unique())
 st.write(f"Se ha seleccionado la asignación {asig}")
 asig_df = prod[prod['Asignación_o_Contrato'] == asig]
 col1, col2 = st.columns(2)
 with col1:
-    anal_p = st.checkbox('Realizar análisis de producción por pozo')
+    anal_p = st.checkbox('Revisar pozo')
     if anal_p:
-        pozo = st.selectbox('Seleccionar pozo a analizar',
+        pozo = st.selectbox('Seleccionar pozo',
                             asig_df['Nombre_del_pozo'].unique())
 with col2:
-    anal = st.button('Analizar')
+    anal = st.button('Revisar')
 st.divider()
 if anal:
-    st.subheader('Producción de petróleo acumulada')
+    st.subheader('Producción de aceite acumulada')
     st.markdown("Producción acumulada por pozo (MMb)")
     acum = asig_df.groupby('Nombre_del_pozo')['Np (MMb)'].sum()
     pozos = asig_df['Nombre_del_pozo'].unique()
     Np_pozos = px.histogram(x=pozos, y=acum, labels={
-        'x': 'Pozos', 'y': 'Np (MMb)'})
+        'x': 'Pozos', 'y': 'Np (MMb)'}, color_discrete_map={'Np (MMb)': '#014135'})
     Np_pozos
     st.markdown(
-        "Cantidad de pozos que han alcanzado cierta producción acumulada (MMb)")
-    Np_count = px.histogram(x=acum, labels={'x': 'Np (MMb)'})
+        "Cantidad de pozos (eje vertical) vs Producción acumulada (MMb)")
+    Np_count = px.histogram(x=acum, labels={'x': 'Np (MMb)'}, color_discrete_map={
+                            'Np (MMb)': '#014135'})
     Np_count
     st.divider()
     st.subheader('Producción de gas acumulada')
     st.markdown("Producción acumulada por pozo (MMMpc)")
     acumg = asig_df.groupby('Nombre_del_pozo')['Gp (MMMpc)'].sum()
     Gp_pozos = px.histogram(x=pozos, y=acumg, labels={
-        'x': 'Pozos', 'y': 'Gp (MMMpc)'})
+        'x': 'Pozos', 'y': 'Gp (MMMpc)'}, color_discrete_map={'Gp (MMMpc)': '#BC8F8F'})
     Gp_pozos
     st.markdown(
-        "Cantidad de pozos que han alcanzado cierta producción acumulada (MMMpc)")
-    Gp_count = px.histogram(x=acumg, labels={'x': 'Gp (MMMpc)'})
+        "Cantidad de pozos (eje vertical) vs Producción acumulada (MMMpc)")
+    Gp_count = px.histogram(x=acumg, labels={'x': 'Gp (MMMpc)'}, color_discrete_map={
+                            'Gp (MMMpc)': '#BC8F8F'})
     Gp_count
     st.divider()
+    st.markdown("Producción de agua por pozo (%)")
+    asig_df['Fecha'] = pd.to_datetime(asig_df['Fecha'], format='%Y-%m-%d')
+    Fw_p = px.scatter(asig_df, x='Fecha', y='Fw (%)', color='Nombre_del_pozo',
+                      color_discrete_sequence=px.colors.sequential.Emrld)
+    Fw_p
+    st.divider()
     if anal_p:
-        st.subheader('Análisis de producción por pozo')
+        st.subheader('Revisión de producción por pozo')
         st.write(f"Se ha seleccionado el pozo {pozo}")
         pozo_df = asig_df[asig_df['Nombre_del_pozo'] == pozo]
-        st.markdown(f"Comportamiento de producción de hidrocarburos")
+        st.markdown("Comportamiento de producción de hidrocarburos")
         compp = px.line(pozo_df, x='Fecha', y=[
-                        'Petróleo_(Mbd)', 'Gas_asociado_(MMpcd)'])
+                        'Petróleo_(Mbd)', 'Gas_asociado_(MMpcd)'], color_discrete_map={'Petróleo_(Mbd)': '#014135', 'Gas_asociado_(MMpcd)': '#BC8F8F'})
         compp
+        st.markdown("Comportamiento de producción de agua")
         pozo_df['Fecha'] = pd.to_datetime(pozo_df['Fecha'], format='%Y-%m-%d')
-        compa = px.scatter(pozo_df, x='Fecha', y='Fw (%)', trendline='lowess')
+        compa = px.scatter(pozo_df, x='Fecha', y='Fw (%)').update_traces(
+            marker=dict(color='turquoise'))
         compa
