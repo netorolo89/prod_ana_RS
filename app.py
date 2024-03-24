4b3d2f626acb715d476ce1ba852362800722c44c
netorolo89 <83844752+netorolo89@users.noreply.github.com>
2024-03-23 19:25:17 -0600

    "analisis pozo"

--- app.py
+++ app.py
@@ -10,11 +10,11 @@ st.subheader('Pendiente por definir...TERMINA AL FINAL')
 st.divider()
 st.markdown("Escribir indicaciones de como interactuar")
 asig = st.selectbox('Seleccionar asignación a analizar',
-                    prod['Asignación_o_Contrato'].unique(), placeholder="Lista de Asignaciones")
+                    prod['Asignación_o_Contrato'].unique())
 st.write(f"Se ha seleccionado la asignación {asig}")
 col1, col2 = st.columns(2)
 with col1:
-    agua = st.checkbox('Incluir comportamiento de agua producida')
+    anal_p = st.checkbox('Realizar análisis de producción por pozo')
 with col2:
     anal = st.button('Analizar')
 st.divider()
@@ -31,6 +31,7 @@ if anal:
         "Cantidad de pozos que han alcanzado cierta producción acumulada (MMb)")
     Np_count = px.histogram(x=acum, labels={'x': 'Np (MMb)'})
     Np_count
+    st.divider()
     st.subheader('Producción de gas acumulada')
     st.markdown("Producción acumulada por pozo (MMMpc)")
     acumg = asig_df.groupby('Nombre_del_pozo')['Gp (MMMpc)'].sum()
@@ -41,3 +42,17 @@ if anal:
         "Cantidad de pozos que han alcanzado cierta producción acumulada (MMMpc)")
     Gp_count = px.histogram(x=acumg, labels={'x': 'Gp (MMMpc)'})
     Gp_count
+    st.divider()
+    if anal_p:
+        st.subheader('Análisis de producción por pozo')
+        pozo = st.selectbox('Seleccionar pozo a analizar', pozos)
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write(f"Se ha seleccionado el pozo {pozo}")
+        with col2:
+            pozoa = st.button('Analizar pozo')
+        # if pozo:
+        # st.markdown(f"Comportamiento de producción del pozo {pozo}")
+        # compp = px.line(asig_df.query("Nombre_del_pozo==pozo"), x='Fecha', y=[
+        #    'Petróleo_(Mbd)', 'Gas_asociado_(MMpcd)'])
+        # compp
